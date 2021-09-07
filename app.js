8aedbe39f2990cfa354387a086c8cbaf913276e4
Mostafa Ayad <mostafaayad67@gmail.com>
2021-09-07 20:40:27 +0200

    connect DB & setup codebase

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+module.exports = app;
