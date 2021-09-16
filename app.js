8a555e51ef1a0937b78fe19f8840d2bf2cd05b38
Ehab <ehabwahba4@gmail.com>
2021-09-16 19:54:31 +0200

    logout error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
-
+const hpp = require('hpp')
 
 const app = express();
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,13 +6,13 @@ const router = express.Router();
 
 router.post('/addUser', authController.addUser);
 router.post('/login', authController.login);
-// router.get('/logout', authController.logout);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-
 router.use(authController.protect);
+
+// router.get('/logout', authController.logout);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 
