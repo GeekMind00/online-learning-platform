eead546c6b59b84a6968fbea82e4895a86a1d0b5
Ehab <ehabwahba4@gmail.com>
2021-10-07 06:41:02 +0200

    notifications

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,8 +1,24 @@
 const Notification = require('../models/notificationModel');
 const factory = require('./handlerFactory');
+const catchAsync = require('./../utils/catchAsync');
 
 
 
-exports.getAllNotifications = factory.getAll(Notification);
+// exports.getAllNotifications = factory.getAll(Notification);
 exports.createNotification = factory.createOne(Notification);
 exports.deleteNotification = factory.deleteOne(Notification);
+
+exports.getAllNotifications = catchAsync(async (req, res, next) => {
+        let notifications = await Notification.find();
+        console.log(notifications)
+        notifications.sort(function (notification1,notification2) {
+            return notification2.createdAt - notification1.createdAt 
+        })
+        // SEND RESPONSE
+        res.status(200).json({
+            status: 'success',
+            data: {
+                notifications
+            }
+        });
+    });
